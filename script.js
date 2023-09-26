67bd86e9b411e062fd5caec486822be5b82b4126
Yusuf Hussein <acedey4you@gmail.com>
2023-09-26 10:35:29 +0100

    modified:   index.html 	modified:   script.js 	modified:   searchmenu.html 	modified:   style.css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const temp = document.getElementById("temp"),
-     date = document.getElementById("date-time"),
+    date = document.getElementById("date-time"),
     currentLocation = document.getElementById("location"),
     condition = document.getElementById("weather-type"),
     windSpeed = document.getElementById("wind-speed"),
@@ -8,11 +8,12 @@ const temp = document.getElementById("temp"),
     visibility = document.getElementById("visibility"),
     humidity_status = document.querySelector('.humidity-status'),
     airPressure = document.getElementById("air-pressure"); 
+    weatherCards = document.getElementById("weather-cards");
 
 
-let currentCity = '';   
-let currentUnit = "c";
-let week = "Week";
+let currentCity; 
+let currentUnit = "°C";
+let hourlyorWeek = "Week";
 
 
 //Update Date-Time
@@ -30,21 +31,30 @@ function getDateTime() {
         'Friday',
         'Saturday',
     ];
-
+    // check for AM and PM
+    let period = "AM";
+    if (hour >= 12) {
+        period = "PM";
+    }
     //12 hour format
     hour = hour % 12;
+    if (hour === 0) {
+        hour = 12;
+    }
+
     if (hour < 10) {
         hour = "0" + hour;
     }
     if (minute < 10) {
         minute = "0" + minute;
     }
-        let dayString = days[now.getDay()];
-        return `${dayString}, ${hour}:${minute}`;
+    
+    let dayString = days[now.getDay()];
+    return `${dayString}, ${hour}:${minute}${period}`;
 }
 
 date.innerText = getDateTime();
-
+// updates the time every second
 setInterval(() => {
     date.innerText = getDateTime();
 }, 1000);
@@ -59,29 +69,30 @@ function getPublicIp() {
     .then((data) => {
         console.log(data);
         currentCity = data.currentCity;
-        getWeatherData(data.city , currentUnit , week);
+        getWeatherData(data.city, currentUnit, hourlyorWeek);
+    })
+    .catch((error) => {
+        console.error("Error fetching geolocation data:", error)
     });
 }
-
 getPublicIp();
 
-//functin to get weather data
- 
-function getWeatherData(city, unit, week) {
+//function to get weather data
+function getWeatherData(city, unit, hourlyorWeek) {
     const apiKey = "FWFMC5LMCS7CLPPCQYULYWCUJ";
+    // "5574fd253b882f1ebbef94f3a1abcc82";
     fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`,
-        {
+        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=${apiKey}&contentType=json`,{
+        // https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
             method: "GET",
-        }
-    )
+        })
         .then((response) => response.json())
         .then((data) => {
             let today = data.currentConditions;
-            if (unit === "c") {
+            if (unit === "°C") {
                 temp.innerText = today.temp;
             } else { 
-                temp.innerText = celciusToFahrenheit(today.temp);
+                temp.innerText = `${celsiusToFahrenheit(today.temp)}`;
             }
             currentLocation.innerText = data.resolvedAddress;
             condition.innerText = today.conditions;
@@ -90,16 +101,20 @@ function getWeatherData(city, unit, week) {
             humidity_status.setAttribute("value", today.humidity);
             humidity_status.setAttribute("style", "--low: 30%; --moderate: 50%; --high: 100%;");
             visibility.innerText = today.visibility;
-            airPressure.innerText = today.winddir; 
+            airPressure.innerText = today.pressure; 
             mainIcon.src = getIcon(today.icon);
+            if (hourlyorWeek === "hourly") {
+                updateForecast(data.days[0].hours, unit, 'day');
+            } else {
+                updateForecast(data.days, unit, 'week');
+            }
         });
 }
 
 //celsius to fahrenheit
-function celciusToFahrenheit(temp) {
+function celsiusToFahrenheit(temp) {
     return ((temp * 9) / 5 + 32).toFixed(1);
 } 
-
 
 function getIcon(condition) {
     if (condition === "Partly-cloudy-day") {
@@ -112,7 +127,7 @@ function getIcon(condition) {
         return "images/mostly_cloudy.png";
     }else if (condition === "light-rain") {
         return "images/light_rain.png";
-    }else if (condition === "fog" || "overcast") {
+    }else if (condition === "fog" || "overcast" || "overcast-clouds") {
         return "images/fog.png";
     }else if (condition === "partly-cloudy-night") {
         return "images/partly_cloudy.png";
@@ -120,4 +135,70 @@ function getIcon(condition) {
         return "images/clear.png";
     }
 }
+
+function getDayName(date) {
+    let day = new Date(date);
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    return days[day.getDay()];
+}
+
+function getHour(time) {
+    let hour = time.split(":")[0];
+    let min = time.split(":")[1];
+    if (hour < 12) {
+        hour = hour - 12;
+        return `${hour}:${min} PM`
+    } else {
+        return `${hour}:${min} AM`;
+    }
+}
+
+function updateForecast(data, unit, type) {
+    weatherCards.innerHTML = '';
+    
+    let day = 0;
+    let numCards = 7;
+    for (let i = 0; i < numCards; i++) {
+        let card = document.createElement("div");
+        card.classList.add("card");        
+    
+    if (type === "week") {
+        dayName = getDayName(data[day].datetime);
+    } 
+    
+    let dayTemp = data[day].temp;
+    if (unit === "°F") {
+        dayTemp = celsiusToFahrenheit(data[day].temp);
+    }
+    let iconCondition = data[day].icon;
+    let iconSrc = getIcon(iconCondition);
+    
+    let tempUnit = "°C";
+        if (unit === "°F") {
+            tempUnit = "°F";
+        }
+ 
+    card.innerHTML = `
+        <h2 class="day-name">${dayName}</h2>
+        <div class="card-icon">
+            <img src="${iconSrc}" alt="">
+        </div>
+        <div class="day-temp">
+            <h2 class="high-temp">${dayTemp}</h2>
+            <span class="temp-unit">${tempUnit}</span>
+        </div>
+        `;
+    weatherCards.appendChild(card);
+    day++;
+    }
+}
+
 
